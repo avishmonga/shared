6d7e9fa5ea8c5ae5b1a9130112925bdfdef5aa91
Avish kumar <avishkmonga@gmail.com>
2024-09-13 15:37:04 +0530

    shared order model for pizza-shop-app

---
+++ pizza-shop-app/models/orderModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const statusEnum = ['pending', 'in-progress', 'completed'];
+const sizeEnum = ['small', 'medium', 'large'];
+const itemTypeEnum = ['pizza', 'soda'];
+
+const itemSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Menu',
+  },
+  name: { type: String, required: true },
+  size: { type: String, enum: sizeEnum, required: true },
+  quantity: { type: Number, required: true },
+  type: { type: String, enum: itemTypeEnum, required: true },
+  price: { type: Number, required: true },
+});
+const orderSchema = new mongoose.Schema({
+  customerName: { type: String, required: true },
+  items: {
+    type: [itemSchema],
+    validate: [arrayLimit, 'At least one item is required'],
+  },
+  status: { type: String, enum: statusEnum, default: 'pending' },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Validator function to ensure at least one item in order should be present before creating
+function arrayLimit(val) {
+  return val.length > 0;
+}
+
+module.exports = mongoose.model('Order', orderSchema, 'orders');
